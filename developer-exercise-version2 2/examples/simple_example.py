d158aeb9408a883e15dd063c1f28b007c3c54815
Elysia Burton <129206950+eburton7@users.noreply.github.com>
2023-04-28 18:12:39 -0400

    Update simple_example.py

--- developer-exercise-version2 2/examples/simple_example.py
+++ developer-exercise-version2 2/examples/simple_example.py
@@ -4,3 +4,22 @@
 
 # Meaningful examples may include reading a file, finding a few nearby points and writing them
 # out to the console.
+
+""" 
+below code is a simple example for the usage of the Nearest Neighbor Index
+"""
+from nearest_neighbor_index import NearestNeighborIndex
+
+# define a list of 2D points
+points = [(1, 1), (2, 3), (4, 2), (5, 6), (6, 1)]
+
+# create an instance of the NearestNeighborIndex class
+nni = NearestNeighborIndex(points)
+
+# define a query point
+query_point = (3, 4)
+
+# find the nearest point to the query point using the find_nearest method
+nearest_point = nni.find_nearest(query_point)
+
+print(f"The nearest point to {query_point} is {nearest_point}")
